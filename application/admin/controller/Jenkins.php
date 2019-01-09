5fdc9df1cf6206ad79e508f4408787ad0e29cf17
wj <1415521548@qq.com>
2019-01-09 17:45:34 +0800

    Jenkins测试

---
+++ application/admin/controller/Jenkins.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangjia
+ * Date: 2019/1/9
+ * Time: 17:43
+ */
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Jenkins extends Controller
+{
+    
+}
